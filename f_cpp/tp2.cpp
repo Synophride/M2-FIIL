55247ba530f84b8bf14165c1d289030303ae22d8
SalleInfo <mjemlajemkhafe@amkka.net>
2019-12-04 18:06:19 +0100

    ajout

--- f_cpp/tp2.cpp
+++ f_cpp/tp2.cpp
@@ -1,10 +1,35 @@
 #include<iostream> 
 
 
-
 class T {
+  T(){
+    std::cout << "Objet créé:\t" << this << std::endl;
+  }
 
-    T(){std::cout<<this<<": constructed from nothing"};
+  T(T &other){
+    std::cout << "Objet créé:\t" << this
+	      << " à partir de " << &other << std::endl;
+    *this=other; 
+  }
+  
+  ~T(){
+    std::cout << "Objet détruit:\t" << this << std::endl;
+  }
 
-    ~T();
-}
+  T& operator=(T& other){
+    std::cout << this << " <- " << &other << std::endl;
+    return other;
+  }
+
+  
+};
+
+
+
+void f1(T const &t) {}
+void f2(T t) {} 
+
+int main(){
+
+
+} 
